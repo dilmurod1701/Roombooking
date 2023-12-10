1c5a54d6cd9c99387176167aeb281c8508efe12e
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-12-10 18:10:39 +0500

    Added: migration path

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('rooms', views.AllRooms.as_view(), name='room-list'),
     path('book/<str:room_name>', views.RoomBooking.as_view(), name='room-booking'),
+    path('migration/', views.migration, name='migration'),
 ]
