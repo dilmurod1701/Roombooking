56cbe7ba79a9397eb5cf88d62e751fc34229bb6d
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-12-10 15:38:31 +0500

    Added: apps in installed apps and changed database to postgresql

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rooms',
+        'USER': 'postgres',
+        'PASSWORD': 'hacker',
+        'HOST': 'localhost',
     }
 }
 
