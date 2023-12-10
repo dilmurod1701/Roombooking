f87ce0d1d35cfd0484a1f7083cf99a9d7f1c5450
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-12-10 18:13:14 +0500

    Added: Login and Signup serializer

--- users/serializers.py
+++ users/serializers.py
@@ -3,3 +3,36 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
 
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(label="Username")
+    password = serializers.CharField(label="Password")
+
+    def validate(self, value):
+        username = value.get('username')
+        password = value.get('password')
+
+        if username and password:
+            user = authenticate(request=self.context.get('request'),
+                                username=username, password=password)
+            if not user:
+                msg = 'Xato: Natogri foydalanuvchi nomini kiritingiz'
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = '"username" va "password" talab qilinadi.'
+            raise serializers.ValidationError(msg, code='authorization')
+        value['user'] = user
+        return value
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
