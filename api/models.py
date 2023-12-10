0d36dccadd34ec7e9ec69777bf12329d007d456f
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-12-10 17:39:22 +0500

    Added: room table

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    xona = models.IntegerField()
+    booked = models.BooleanField(default=False)
+    start = models.DateTimeField(null=True, blank=True)
+    end = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
