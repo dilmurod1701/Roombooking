393ef21e99b814f2c25b193237fc094b37a2568e
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-12-10 18:03:23 +0500

    Added: UserManager

--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone, password=None):
+        if not phone:
+            raise ValueError(_('Phone number must be entered'))
+        user = self.model(
+            phone=phone
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, password=None):
+        user = self.create_user(
+            phone=phone,
+            password=password
+        )
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
