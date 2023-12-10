121fea9c77bc2dc0f73802fac7a97305bdaee585
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-12-10 18:05:51 +0500

    Added: CustomUserAdmin class to customize admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+from .forms import FormUserChange, CreateUserForm
+
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CreateUserForm
+    form = FormUserChange
+    model = User
+    list_display = ['phone', 'is_active', 'is_staff']
+    list_filter = ['phone']
+    fieldsets = [
+        (None, {'fields': ['phone', 'password']}),
+        ('Personal info', {'fields': ['first_name', 'last_name']}),
+        ('Permission', {'fields': ['is_staff']})
+    ]
+
+    add_fieldsets = [
+        (
+            None,
+            {
+                'fields': ['phone', 'password1', 'password2']
+            },
+        ),
+    ]
+    search_fields = ['phone']
+    ordering = ['phone']
+
+
+admin.site.register(User, CustomUserAdmin)
